b21b8ed45ed929b3d986f2cff447aa818f142ae3
Ramón Arenal <ramon.arenal@dynatrace.com>
2022-01-12 16:06:19 +0100

    Created base AreaChart component

---
+++ src/components/Visualizations/AreaChart.tsx
@@ -0,0 +1,89 @@
+import { AxisBottom, AxisLeft, AxisScale } from '@visx/axis';
+import { curveMonotoneX } from '@visx/curve';
+import { LinearGradient } from '@visx/gradient';
+import React, { PropsWithChildren } from 'react';
+import { GraphDatapoint } from './Graph';
+import { Group } from '@visx/group';
+import { AreaClosed } from '@visx/shape';
+
+interface AreaChartProps {
+  data: GraphDatapoint[];
+  gradientColor: string;
+  xScale: AxisScale<number>;
+  yScale: AxisScale<number>;
+  width: number;
+  yMax: number;
+  margin: { top: number; right: number; bottom: number; left: number };
+  hideBottomAxis?: boolean;
+  hideLeftAxis?: boolean;
+  top?: number;
+  left?: number;
+}
+
+const getDate = (datapoint: GraphDatapoint) => datapoint.timestamp;
+const getValue = (datapoint: GraphDatapoint) => datapoint.value;
+
+const axisColor = '#fff';
+const axisBottomTickLabelProps = {
+  textAnchor: 'middle' as const,
+  fontFamily: 'Arial',
+  fontSize: 10,
+  fill: axisColor,
+};
+const axisLeftTickLabelProps = {
+  dx: '-0.25em',
+  dy: '0.25em',
+  fontFamily: 'Arial',
+  fontSize: 10,
+  textAnchor: 'end' as const,
+  fill: axisColor,
+};
+
+export const AreaChart = ({
+  data,
+  gradientColor,
+  width,
+  yMax,
+  margin,
+  xScale,
+  yScale,
+  hideBottomAxis = false,
+  hideLeftAxis = false,
+  top,
+  left,
+  children
+}: PropsWithChildren<AreaChartProps>) => {
+  return (
+    <Group left={left || margin.left} top={top || margin.top}>
+      <LinearGradient id="gradient" from={gradientColor} fromOpacity={1} to={gradientColor} toOpacity={0.2} />
+      <AreaClosed<GraphDatapoint>
+        data={data}
+        x={(d) => xScale(getDate(d)) || 0}
+        y={(d) => yScale(getValue(d)) || 0}
+        yScale={yScale}
+        strokeWidth={1}
+        stroke="url(#gradient)"
+        fill="url(#gradient)"
+        curve={curveMonotoneX}
+      />
+      {!hideBottomAxis && (
+        <AxisBottom
+          top={yMax}
+          scale={xScale}
+          numTicks={width > 520 ? 10 : 5}
+          stroke={axisColor}
+          tickStroke={axisColor}
+          tickLabelProps={() => axisBottomTickLabelProps} />)
+      }
+      {!hideLeftAxis && (
+        <AxisLeft
+          scale={yScale}
+          numTicks={5}
+          stroke={axisColor}
+          tickStroke={axisColor}
+          tickLabelProps={() => axisLeftTickLabelProps} />)
+      }
+      {children}
+    </Group>
+  );
+};
